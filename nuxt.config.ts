9884e0328ee9c2f8fde6af63a773a03edf00b710
Matej Pangerc <matej@idoru.ai>
2024-01-29 10:59:10 +0400

    favicon and page title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      title: 'DC3 - Deep Conviction Capital',
+      link: [
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon/favicon-16x16.png' },
+        { rel: 'manifest', href: '/favicon/site.webmanifest' },
+        { rel: 'mask-icon', href: '/favicon/safari-pinned-tab.svg', color: '#5bbad5' },
+      ],
+      meta: [
+        { name: 'msapplication-TileColor', content: '#da532c' },
+        { name: 'theme-color', content: '#ffffff' },
+      ],
+    },
+  },
   css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
